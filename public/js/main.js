e215f9ba9db6555b48cf0fee79f24d716fd6880f
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-03 13:11:54 -0400

    second button done

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,16 @@
 (function () {
     const buttons = document.querySelectorAll('button')
     const firstButton = buttons[0]
+    const secondButton = buttons[1]
+    const thirdButton = buttons[2]
 
-    firstbutton.addEventListener('click', function () {
+    firstButton.addEventListener('click', function () {
       firstButton.classList.toggle('button-danger');
     })
 
-}
+    secondButton.addEventListener('click', function () {
+      secondButton.innerText = 'you clicked me';
+    })
 
-
+    
 })();
